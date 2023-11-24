10690ddcd221de7c3fafef4ba67a4b8eaf4f96bd
Hanqi0503 <han.qi2@northeastern.edu>
2023-11-24 15:59:02 -0500

    Initial commit

---
+++ src/Singleton/SingleObject.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Singleton;
+
+/**
+ *
+ * @author hanqi
+ */
+public class SingleObject {
+
+    static SingleObject getInstance() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void showMessage() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
